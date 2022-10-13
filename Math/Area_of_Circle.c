eead2127760177a314678f50cffa935f5a5ad8b4
Cindy Thai <99574378+CindCodes@users.noreply.github.com>
2022-10-13 07:39:01 -0400

    Create Area_of_Circle.c

---
+++ Math/Area_of_Circle.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define PI 3.14
+int main(){
+double radius, BaSe, HEIGHT; 
+printf("Enter the radius of the circle:\n"); scanf("%d",&radius);
+float area;
+    area=PI * radius * radius;
+printf("The area of the circle=%f\n",&area); 
+return 0;
+}
