4b5f68d45c232ed5e45b14754ae4cfd9d98a9192
Cindy Thai <99574378+CindCodes@users.noreply.github.com>
2022-10-13 07:30:58 -0400

    Create Three_Number_Largest.c

---
+++ Conditional_Statement/Three_Number_Largest.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+  int num1=10,num2=20,num3=30;
+  if(num2>num1){
+    if(num3>2){
+      printf("num3 largest");
+    }
+    else{
+      printf("num2 largest");
+    }
+  }
+  return 0;
+	}
